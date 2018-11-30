453e7ddc2c3d5d833a4fccc7f9b871fb7f4179e6
eldar.jah <eldar.jah@gmail.com>
2018-11-30 13:08:01 +0100

    Adding and updating restoran info and likes models.

--- app/src/main/java/com/eldar/fit/seminarski/helper/RestoranInfo.java
+++ app/src/main/java/com/eldar/fit/seminarski/helper/RestoranInfo.java
@@ -1,4 +1,45 @@
 package com.eldar.fit.seminarski.helper;
 
+import com.eldar.fit.seminarski.data.TipKuhinje;
+import com.eldar.fit.seminarski.data.Vlasnik;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
 public class RestoranInfo {
+    public int id;
+    public String naziv;
+    public String opis;
+    public Vlasnik vlasnik;
+    public String telefon;
+    public String lokacija;
+    public List<RestoranLike> lajkovi;
+    public String slika;
+    public String slogan;
+
+    @SerializedName(value = "webUrl", alternate = {"WebUrl", "weburl"})
+    public String webUrl;
+
+    @SerializedName(value = "tipoviKuhinje", alternate = {"TipoviKuhinje", "tipovikuhinje"})
+    public List<TipKuhinje> tipoviKuhinje;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNaziv() {
+        return naziv;
+    }
+
+    public String getOpis() {
+        return opis;
+    }
+
+    public String getSlika() {
+        return "http://" + slika;
+    }
+
+    public int getLikesCount() {
+        return lajkovi.size();
+    }
 }
--- app/src/main/java/com/eldar/fit/seminarski/helper/RestoranLike.java
+++ app/src/main/java/com/eldar/fit/seminarski/helper/RestoranLike.java
@@ -1,4 +1,12 @@
 package com.eldar.fit.seminarski.helper;
 
+import com.google.gson.annotations.SerializedName;
+
+import java.util.Date;
+
 public class RestoranLike {
+    @SerializedName(value = "imePrezime", alternate = {"ImePrezime", "imeprezime"})
+    public String imePrezime;
+    public String recenzija;
+    public Date datum;
 }
