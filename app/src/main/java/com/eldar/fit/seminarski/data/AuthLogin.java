f2575fb171e4a99d6c9bf09ad996f3c7daa9d63d
eldar.jah <eldar.jah@gmail.com>
2018-11-30 20:10:40 +0100

    Added credentials model for using it in api request (login) user.

--- app/src/main/java/com/eldar/fit/seminarski/data/AuthLogin.java
+++ app/src/main/java/com/eldar/fit/seminarski/data/AuthLogin.java
@@ -1,4 +1,11 @@
 package com.eldar.fit.seminarski.data;
 
 public class AuthLogin {
+    public String username;
+    public String password;
+
+    public AuthLogin(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
 }
