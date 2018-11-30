d4336a0342e006ebdcd247d63ff5dffb1c3704dc
eldar.jah <eldar.jah@gmail.com>
2018-11-30 13:05:52 +0100

    Adding api handlers.

--- app/src/main/java/com/eldar/fit/seminarski/helper/MyApiRequest.java
+++ app/src/main/java/com/eldar/fit/seminarski/helper/MyApiRequest.java
@@ -1,4 +1,77 @@
 package com.eldar.fit.seminarski.helper;
 
+import android.app.Activity;
+import android.os.AsyncTask;
+import android.support.design.widget.Snackbar;
+import android.util.Log;
+import android.view.View;
+
+import com.eldar.fit.seminarski.R;
+
+import java.lang.reflect.Type;
+
 public class MyApiRequest {
+
+    public static String CONTENT_TYPE_JSON = "application/json";
+    public static String ENDPOINT_RESTORANI = "restorani";
+    public static String ENDPOINT_AUTH = "auth";
+
+    public static <T> void request(final Activity activity, final String endpoint, final MyUrlConnection.HttpMethod httpMethod, final Object postObject, final MyAbstractRunnable<T> callback) {
+        new AsyncTask<Void, Void, MyApiResult>() {
+
+            @Override
+            protected MyApiResult doInBackground(Void... voids)
+            {
+                try {Thread.sleep(1000);} catch (Exception e ) {}
+                String jsonPostObject = postObject == null ? null : MyGson.build().toJson(postObject);
+                MyApiResult result = MyUrlConnection.request(MyConfig.apiBase + "/" + endpoint, httpMethod, jsonPostObject, CONTENT_TYPE_JSON);
+                return result;
+            }
+
+            @Override
+            protected void onPostExecute(MyApiResult result)
+            {
+                View parentLayout = activity.findViewById(R.id.content);
+                Snackbar snackbar = null;
+                if (result.isException) {
+
+                    if (result.resultCode == 0) {
+                        Log.i("Test", "asyncApiRequest - error server communication.");
+                        snackbar = Snackbar.make(parentLayout, "Greška u komunikaciji sa serverom. ", Snackbar.LENGTH_LONG);
+                    } else {
+                        Log.i("Test", "asyncApiRequest - error:" + result.errorMessage + " statuscode:" + result.resultCode);
+                        snackbar = Snackbar.make(parentLayout, "Greška " + result.resultCode + ": " + result.errorMessage, Snackbar.LENGTH_LONG);
+                    }
+                }
+                else {
+                    Type genericType = callback.getGenericType();
+
+                    try {
+                        T mappedObj = MyGson.build().fromJson(result.value, genericType);
+                        callback.run(mappedObj);
+                    } catch (Exception e) {
+                        Log.i("Test", "asyncApiRequest - get - Exception:" + e.getMessage());
+                        snackbar = Snackbar.make(parentLayout, "Greška pri prikazivanju podataka. ", Snackbar.LENGTH_LONG);
+                    }
+                }
+                if (snackbar != null) {
+                    snackbar.setAction("Učitaj ponovo", new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            MyApiRequest.get(activity, endpoint, callback);
+                        }
+                    });
+                    snackbar.show();
+                }
+            }
+        }.execute();
+    }
+
+    public static <T> void get(final Activity activity, final String endpoint, final MyAbstractRunnable<T> callback){
+        request(activity, endpoint, MyUrlConnection.HttpMethod.GET, null, callback);
+    }
+
+    public static <T> void post(final Activity activity, final String endpoint, Object postObject, final MyAbstractRunnable<T> callback){
+        request(activity, endpoint, MyUrlConnection.HttpMethod.POST, postObject, callback);
+    }
 }
--- app/src/main/java/com/eldar/fit/seminarski/helper/MyApiResult.java
+++ app/src/main/java/com/eldar/fit/seminarski/helper/MyApiResult.java
@@ -1,4 +1,28 @@
 package com.eldar.fit.seminarski.helper;
 
 public class MyApiResult {
+    public String errorMessage = "";
+    public boolean isException = false;
+    public int resultCode = 0;
+    public String value;
+
+    public static MyApiResult OK(String value) {
+        MyApiResult result = new MyApiResult();
+
+        result.value = value;
+        result.isException = false;
+        result.resultCode = 200;
+        result.errorMessage = "";
+
+        return result;
+    }
+
+    public static MyApiResult Error(int exceptionCode, String exceptionMessage) {
+        MyApiResult result = new MyApiResult();
+        result.isException = true;
+        result.resultCode = exceptionCode;
+        result.errorMessage = exceptionMessage;
+
+        return result;
+    }
 }
