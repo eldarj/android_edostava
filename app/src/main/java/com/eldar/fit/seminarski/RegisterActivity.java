227c386edd43a25795ffd8d87475176e1812fb8c
eldar.jah <eldar.jah@gmail.com>
2018-12-05 21:02:40 +0100

    Improving register activity, cleaning up layout and tweaking dimens and strings resources.

--- app/src/main/java/com/eldar/fit/seminarski/RegisterActivity.java
+++ app/src/main/java/com/eldar/fit/seminarski/RegisterActivity.java
@@ -6,9 +6,9 @@ import android.support.annotation.Nullable;
 import android.support.design.widget.Snackbar;
 import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.Spinner;
 
 import com.eldar.fit.seminarski.data.ApiBlokList;
@@ -23,7 +23,6 @@ import com.eldar.fit.seminarski.helper.MyUtils;
 
 import java.util.List;
 
-import static com.eldar.fit.seminarski.helper.MyApiRequest.ENDPOINT_USER_REGISTER_AUTH;
 import static com.eldar.fit.seminarski.helper.MyApiRequest.ENDPOINT_LOCATIONS;
 
 public class RegisterActivity extends AppCompatActivity {
@@ -31,11 +30,16 @@ public class RegisterActivity extends AppCompatActivity {
     private TextInputEditText inputUsername, inputPassword, inputIme, inputPrezime;
     private Spinner spinnerBlok;
     private Button btnRegister, btnOpenLogin;
+    private ProgressBar progressBar_blokSpinner;
+    private ProgressBar progressBar_login;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        progressBar_blokSpinner = findViewById(R.id.progressBar_blokSpinner);
+        progressBar_login = findViewById(R.id.progressBar_login);
 
         inputUsername = findViewById(R.id.inputRegisterUsername);
         inputPassword = findViewById(R.id.inputRegisterPassword);
@@ -59,7 +63,11 @@ public class RegisterActivity extends AppCompatActivity {
         });
 
         spinnerBlok = findViewById(R.id.spinnerRegisterBlok);
-        MyApiRequest.get(this, ENDPOINT_LOCATIONS, new MyAbstractRunnable<ApiBlokList>() {
+        blokPodaciRequest();
+    }
+
+    private void blokPodaciRequest() {
+        MyApiRequest.get(ENDPOINT_LOCATIONS, new MyAbstractRunnable<ApiBlokList>() {
             @Override
             public void run(ApiBlokList apiBlokList) {
                 List<String> blokPodaci = ApiResponseHandler.getStringListBlokovi(apiBlokList);
@@ -72,13 +80,10 @@ public class RegisterActivity extends AppCompatActivity {
                 onBlokPodaciReceived(null, statusCode, errorMessage);
             }
         });
-
     }
 
     private void onBlokPodaciReceived(@Nullable List<String> blokPodaci, @Nullable Integer statusCode, @Nullable String errorMessage) {
-        if (findViewById(R.id.progressBar_blokSpinner) != null) {
-            findViewById(R.id.progressBar_blokSpinner).setVisibility(View.INVISIBLE);
-        }
+        progressBar_blokSpinner.setVisibility(View.INVISIBLE);
 
         if (blokPodaci != null) {
             MyUtils.popuniSpinner(this,
@@ -88,8 +93,15 @@ public class RegisterActivity extends AppCompatActivity {
                     spinnerBlok);
         } else {
             Snackbar.make(findViewById(android.R.id.content),
-                    errorMessage != null ? errorMessage : "Dogodila se greška.",
-                    Snackbar.LENGTH_LONG).show();
+                    errorMessage != null ? errorMessage : getString(R.string.dogodila_se_greska),
+                    Snackbar.LENGTH_INDEFINITE)
+                    .setAction(R.string.api_load_ponovo, new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            blokPodaciRequest();
+                        }
+                    })
+                    .show();
         }
     }
 
@@ -98,24 +110,24 @@ public class RegisterActivity extends AppCompatActivity {
     }
 
     private void do_btnRegisterClick() {
-        if (inputPassword == null || inputPassword.length() <= 4) {
+        if (inputPassword.length() <= 4) {
             inputPassword.setError(getString(R.string.register_error_password));
         } else {
             inputPassword.setError(null); // Clear the error
         }
-        if (inputUsername == null || inputUsername.length() <= 4) {
+        if (inputUsername.length() <= 4) {
             inputUsername.setError(getString(R.string.register_error_username));
         } else if(Storage.UsernameExists(inputUsername.getText().toString())) {
             inputUsername.setError(getString(R.string.register_error_username_taken));
         } else {
             inputUsername.setError(null); // Clear the error
         }
-        if (inputIme == null || inputIme.length() <= 4) {
+        if (inputIme.length() <= 4) {
             inputIme.setError(getString(R.string.register_error_ime));
         } else {
             inputIme.setError(null); // Clear the error
         }
-        if (inputPrezime == null || inputPrezime.length() <= 4) {
+        if (inputPrezime.length() <= 4) {
             inputPrezime.setError(getString(R.string.register_error_prezime));
         } else {
             inputPrezime.setError(null); // Clear the error
@@ -125,7 +137,7 @@ public class RegisterActivity extends AppCompatActivity {
                 || inputPassword.getError() != null
                 || inputIme.getError() != null
                 || inputPassword.getError() != null) {
-            Snackbar.make(findViewById(android.R.id.content), "Molimo provjerite podatke!", Snackbar.LENGTH_LONG).show();
+            Snackbar.make(findViewById(android.R.id.content), getString(R.string.input_podaci_invalid), Snackbar.LENGTH_SHORT).show();
             return;
         }
 
@@ -140,10 +152,9 @@ public class RegisterActivity extends AppCompatActivity {
                 ApiResponseHandler.getBlokovi().get(spinnerBlok.getSelectedItemPosition()).getId()
         );
 
-        MyApiRequest.post(this, ENDPOINT_USER_REGISTER_AUTH, newAccount, new MyAbstractRunnable<KorisnikVM>() {
+        MyApiRequest.post(MyApiRequest.ENDPOINT_USER_REGISTER_AUTH, newAccount, new MyAbstractRunnable<KorisnikVM>() {
             @Override
             public void run(KorisnikVM korisnikVM) {
-                Log.i("Test", "run, result: " + korisnikVM.getUsername());
                 loginUser(korisnikVM, null, null);
             }
 
@@ -154,14 +165,15 @@ public class RegisterActivity extends AppCompatActivity {
         });
     }
     private void loginUser(@Nullable KorisnikVM korisnik, @Nullable Integer statusCode, @Nullable String errorMessage) {
-        if (findViewById(R.id.progressBar_login) != null) {
-            findViewById(R.id.progressBar_login).setVisibility(View.INVISIBLE);
-        }
+        progressBar_login.setVisibility(View.INVISIBLE);
+
         if (korisnik == null) {
-            Snackbar.make(findViewById(android.R.id.content), errorMessage != null ? errorMessage : "Pogrešan username ili password." , Snackbar.LENGTH_LONG).show();
+            Snackbar.make(findViewById(android.R.id.content),
+                    errorMessage != null ? errorMessage : getString(R.string.input_podaci_invalid) , Snackbar.LENGTH_SHORT).show();
         } else {
             MySession.setKorisnik(korisnik);
-            Snackbar.make(findViewById(android.R.id.content), "Uspješno ste se registrovali!" , Snackbar.LENGTH_LONG).show();
+            Snackbar.make(findViewById(android.R.id.content), getString(R.string.uspjesna_registracija) , Snackbar.LENGTH_SHORT).show();
+
             startActivity(new Intent(this, GlavniActivity.class));
             finish();
         }
