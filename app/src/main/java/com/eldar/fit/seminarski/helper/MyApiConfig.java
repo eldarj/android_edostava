deda8da489a56b56596f16d77dbeb054218a2c22
Eldar Jahijagić <eldar.jahijagic@edu.fit.ba>
2018-12-21 07:48:42 +0100

    Final tweaks.

--- app/src/main/java/com/eldar/fit/seminarski/helper/MyApiConfig.java
+++ app/src/main/java/com/eldar/fit/seminarski/helper/MyApiConfig.java
@@ -4,7 +4,7 @@ import android.net.Uri;
 
 public class MyApiConfig {
 
-    public static final String APP_BASE = "http://192.168.1.3:51627/";
+    public static final String APP_BASE = "https://edostava.azurewebsites.net/";
 
     public static final Uri apiBase = Uri.parse(APP_BASE + "api");
 
